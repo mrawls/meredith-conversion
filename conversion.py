30844faa267a5b91ae0416f57ec0a975519d1652
Michael Bigelow <Michael.o.Bigelow@gmail.com>
2018-04-14 17:41:22 -0700

    Created tutorial python functions in conversion.py. This was from the ToDo.txt file found on 2018/04/14 on the github repo.

--- conversion.py
+++ conversion.py
@@ -3,3 +3,19 @@
 def dollars2cents(dollars):
     cents = dollars * 100
     return cents
+
+def Gallos2Liters(gallons):
+    liter = gallons * 3.78541
+    return liter
+
+def hours2minutes(hours):
+    minutes = hours * 60
+    return minutes
+
+def feet2inches(feet):
+    inches = feet * 12
+    return Inches
+
+def Degrees2Radians(degrees):
+    radians = pi() * degrees / 180
+    return radians
